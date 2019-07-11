acc6904434e23fae59eb579b2437124e8d6eba3c
moho23 <m_hosseinpour10@yahoo.com>
2019-07-11 16:57:08 +0430

    some changes and some new files added

---
+++ Server/src/Menu.java
@@ -0,0 +1,48 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Menu implements Initializable {
+
+    @FXML
+    Button backButton;
+
+    @FXML
+    Button LoginButton;
+
+    @FXML
+    Button SignupButton;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        backButton.setOnAction(event -> {
+            try {
+                Server.stage.setScene(FXMLLoader.load(getClass().getResource("Start.fxml")));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        LoginButton.setOnAction(event -> {
+            try {
+                Server.stage.setScene(FXMLLoader.load(getClass().getResource("Login.fxml")));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        SignupButton.setOnAction(event -> {
+            try {
+                Server.stage.setScene(FXMLLoader.load(getClass().getResource("Signup.fxml")));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
--- Server/src/Start.java
+++ Server/src/Start.java
@@ -1,11 +1,10 @@
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
-
-import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,15 +12,15 @@ import java.util.ResourceBundle;
 public class Start implements Initializable {
 
     @FXML
-    Button start;
+    Button startPage;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        start.setOnAction(event -> {
+        startPage.setOnAction(event -> {
             try {
-                Server.stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Menu.fxml"))));
+                Server.stage.setScene(FXMLLoader.load(getClass().getResource("Menu.fxml")));
             } catch (IOException e) {
-                e.printStackTrace();
+                e.getMessage();
             }
         });
     }
